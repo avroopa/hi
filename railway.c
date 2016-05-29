0a3842c5f450ce947d618b835c0d9fea00d2ee44
avroopa <apoorvarawat2015@gmail.com>
2016-05-29 22:07:08 +0530

    Add files via upload

--- railway.c
+++ railway.c
@@ -1,16 +1,121 @@
 #include<stdio.h>
+#include<string.h>
 struct air
-{ char name[20];
+{ 
   int phno;
   char address[50];
   int ticketno;
+  char name[30],pass[30];
 
-  }s;
+  }s[99];
   void menu();
   void view();
   void cancel();
   void reserve();
+  void login();
+  void reg();
+  int i;
   
+  void reg()
+  {
+    FILE *fp;
+    char c,checker[30]; int i,n,z=0;
+    fp=fopen("Web_reg.txt","ab+");
+    for(i=0;i<100;i++)
+    {
+      printf("\n ENTER USERNAME: ");
+      scanf("%s",checker);
+        while(!feof(fp))
+        {
+          fread(&s[i],sizeof(s[i]),1,fp);
+          if(strcmp(checker,s[i].name)==0)
+            {
+            printf("\nUSERNAME ALREADY EXISTS");
+           
+            reg();
+            }
+          else
+          {
+            strcpy(s[i].name,checker);
+            break;
+          }
+        }
+      printf("\n  ENTER PASSWORD: ");
+      while((c=getch())!=13)
+        {
+          s[i].pass[z++]=c;
+          printf("%c",'*');
+        }
+      fwrite(&s[i],sizeof(s[i]),1,fp);
+      fclose(fp);
+      printf("\nPress enter if you agree with Username and Password");
+      if((c=getch())==13)
+        {
+      
+        printf("\nYou are successfully registered");
+        printf("\nTry login your account??");
+        printf("> PRESS 1 FOR YES\n > PRESS 2 FOR NO");
+        scanf("%d",&n);
+        switch(n)
+          {
+              case 1: 
+                    login();
+                    break;
+              case 2: 
+                    printf("\nTHANK YOU FOR REGISTERING");
+                    break;
+          }
+        }
+        break;
+      }
+
+  }
+  void login()
+    {
+      FILE *fp;
+      char c,name[30],pass[30]; int i,z=0,y;
+      int checku,checkp;
+      fp=fopen("Web_reg.txt","rb");
+      for(i=0;i<1000;i++)
+      {
+        printf("\nENTER USERNAME: ");
+        scanf("%s",name);
+        printf("\nENTER PASSWORD: ");
+        while((c=getch())!=13)
+        {
+          pass[z++]=c;
+          printf("%c",'*');
+        }
+        pass[z]='\0';
+      while(!feof(fp))
+        {
+        fread(&s[i],sizeof(s[i]),1,fp);
+          checku=strcmp(name,s[i].name);
+          checkp=strcmp(pass,s[i].pass);
+          if(checku==0&&checkp==0)
+          {
+
+            printf("\nYOU HAVE LOGGED IN SUCCESSFULLY!!");
+            
+            break;
+          }
+        if(checku==0&&checkp!=0)
+          {
+            printf("\nWRONG PASSWORD!! Not %s??",name);
+            printf("\n(Press 'Y' to re-login)");
+            scanf("%s",&y);
+            if(y=='y'||y=='Y')
+              login();
+          }
+        if(checku!=0)
+          {
+            printf("\nYou are not a Registered User\n Press enter to register yourself");
+            reg();
+          }
+        }
+        break;
+      }
+    }
 void menu()
 { int ch;
 printf("\n\n1.VIEW ALL TRAINS");
@@ -48,57 +153,53 @@ void reserve()
   
   if(reserved<total_seats)
   {  reserved++;
-   printf("Enter the train code");
+   printf("\nEnter the train code");
    scanf("%d",&code);
    if(code==1021||code==1024||code==1098||code==1987||code==1576)
     { 
-     printf("\n 1.first class(fare Rs1500 )");
-     printf("\n 2.second class(fare Rs1000)");
-     printf("\n 3.sleeper class(fare Rs500)");
+     printf("\n1.first class(fare Rs1500 )");
+     printf("\n2.second class(fare Rs1000)");
+     printf("\n3.sleeper class(fare Rs500)");
      scanf("%d",&class);
-     printf("Enter number of tickets to be booked");
-     scanf("%d",&no);
+     printf("\nEnter number of tickets to be booked");
+     scanf("%d",&s[i].ticketno);
        if(class==1)
        {
        	
-		a = 1500*no;
- printf("Your fare is Rs.%d",&a);
+		a = 1500 * s[i].ticketno;
+ printf("\nYour fare is Rs.%d",a);
  
        }
        else if(class==2)
        {
  
-		a = no*1000;
- printf("Your fare is Rs.%d",&a);
+		a = 1000 * s[i].ticketno;
+ printf("\nYour fare is Rs.%d",a);
        }
        else if(class==3)
        {
        	
-		a = 500*no;
- printf("Your fare is Rs.%d",&a);
+		a = 500 * s[i].ticketno;
+ printf("\nYour fare is Rs.%d",a);
        }
        else
        {
-	     printf("Enter valid choice(1,2or 3)");
+	     printf("\nEnter valid choice(1,2or 3)");
        }
     
- printf("\nEnter the name");
- scanf("%s",&s.name);
  printf("\nEnter the phone number");
- scanf("%d",&s.phno);
+ scanf("%d",&s[i].phno);
  printf("\n Enter the address");
- scanf("%s",&s.address);
-  printf("Number of tickets");
-  scanf("%d",&s.ticketno);
- printf("Your ticket is confirmed");
+ scanf("%s",&s[i].address);
+ printf("\nYour ticket is confirmed");
  
  printf("\nHere is your ticket\n");
  printf("-----------------------------------------------------------------------------------------------\n");
  printf("                             APOORVA'S RAILWAY NETWORK                                        \n");
- printf(" Name: %c                                                                                     \n",&s.name);
- printf(" Phone Number: %d                                                                             \n",&s.phno);
- printf(" Address: %c                                                                                  \n",&s.address);                   
- printf(" Tickets Booked: %d                                                                           \n",&s.ticketno);
+ printf(" Name: %c                                                                                     \n",s[i].name);
+ printf(" Phone Number: %d                                                                             \n",s[i].phno);
+ printf(" Address: %c                                                                                  \n",s[i].address);                   
+ printf(" Tickets Booked: %d                                                                           \n",s[i].ticketno);
  printf("                               HAVE A NICE JOURNEY!                                          \n");
  printf("-----------------------------------------------------------------------------------------------\n");
 
@@ -114,7 +215,7 @@ void cancel()
  
 printf("enter the ticket no");
 scanf("%d",&ticket);
- if(ticket==s.ticketno)
+ if(ticket==s[i].ticketno)
   {    printf("your ticket is cancelled");
 
    }
@@ -124,9 +225,24 @@ scanf("%d",&ticket);
  menu();
 }}
 void main(){
+	int n;
 	printf("***********************************************************");
 	printf("\n         Welcome to Apoorva's Railway Network              ");
 	printf("\n***********************************************************");
+	printf("\n1. LOGIN \n2. REGISTER");
+scanf("%d",&n);
+switch(n)
+  {
+    case 1: 
+	       login();
+        break;
+    case 2: 
+        reg();
+        break;
+    default: printf("\nNO MATCH FOUND");
+        printf("\nPress Enter to re-Enter the choice");
+        
+  }
 	
  menu();
 }
